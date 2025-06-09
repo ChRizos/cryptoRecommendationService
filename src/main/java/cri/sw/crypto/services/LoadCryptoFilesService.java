bad825b7412c3c8a6acc6c10497f4d26897ec20f
christos <christosprizos@gmail.com>
2025-06-10 00:47:18 +0300

    feat: changed the hardcoded folder path value to read the path from .properties file

--- src/main/java/cri/sw/crypto/services/LoadCryptoFilesService.java
+++ src/main/java/cri/sw/crypto/services/LoadCryptoFilesService.java
@@ -7,6 +7,7 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -30,11 +31,13 @@ public class LoadCryptoFilesService {
         return cryptoData.getOrDefault(symbol.toUpperCase(), Collections.emptyList());
     }
 
+    @Value("${crypto.data.dir}")
+    String relativeFolderPath;
 
     @PostConstruct
     public void init() throws IOException {
         try {
-            String relativeFolderPath = "./data";
+
             List<Resource> resources = getCsvResourcesFromRelativePath(relativeFolderPath);
 
             for (Resource resource : resources) {
